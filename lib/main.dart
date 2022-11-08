766b01b721ecce55b294c933d345c546d7fa9164
joshuapurbaa <yoshuapurba06@gmail.com>
2022-11-08 16:57:38 +0700

    Add view_model and res folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:alterra_chat/views/sign_in_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:responsive_framework/responsive_framework.dart';
 
 import 'res/res.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const AlterraChat());
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
